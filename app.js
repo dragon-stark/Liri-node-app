e493a091c2b7445e21a67ebe200e4bb932e0fe08
DStark <hindash@email.arizona.edu>
2019-12-06 19:45:20 -0700

    uncommented functions

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
-// require("dotenv").config;
+require("dotenv").config;
 var fs = require("fs");
 var keys = require("./key");
 var request = require("request");
 var Spotify = require("node-spotify-api");
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 // command line arguments
 var action = process.argv[2];
