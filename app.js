ec2f99089b8d8160cb2e902e410486b7b78e8bd6
DStark <hindash@email.arizona.edu>
2019-12-13 19:25:50 -0700

    prince saves the app

--- app.js
+++ app.js
@@ -1,16 +1,64 @@
 
-require("dotenv").config();
+// require("dotenv").config();
 var fs = require("fs");
-var keys = require("./keys");
+// var keys = require("./keys");
 var request = require("request");
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
+// var Spotify = require("node-spotify-api");
+// var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
+var moment = require("moment");
+
 // command line arguments
 var action = process.argv[2];
 var parameter = process.argv[3];
 
-function Search (concert, song, movie, action)
+
 // node liri.js concert - this < artist / band name here >
 
-//   This will search the Bands in Town Artist Events API("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+// begin api function for concert-this
+var bandsintown = function (artist)
+{
+  // var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+  var queryURL = "https://rest.bandsintown.com/artists/jlo/events?app_id=codingbootcamp";
+
+  // begin axios wrapper
+  axios.get(queryURL).then(
+    function (response)
+    {
+      var jsonData = response.data;
+
+      if (!jsonData.length) {
+        console.log("Sorry no information available for " + artist);
+        return;
+      }
+      console.log("appearences for " + artist + " near you:");
+      for (var i = 0; i < jsonData.length; i++) {
+        var band = jsonData[i];
+
+        console.log(
+          band.venue.city +
+          "," + (band.venue.region || band.venue.country) + " at " + band.venue.name + " " + moment(band.datetime).format("MM/DD/YYYY")
+        )
+      }
+    }
+  )
+}
+
+var select = function (caseData, functionData)
+{
+  switch (caseData) {
+    case "concert-this": bandsintown(functionData);
+      break;
+    default: console.log("Error")
+  }
+};
+
+var execute = function (action, parameter)
+{
+  select(action, parameter)
+};
+
+execute(process.argv[2], process.argv.slice(3).join(" "));
+
+//   This will search the Bands in Town Artist bands API("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
