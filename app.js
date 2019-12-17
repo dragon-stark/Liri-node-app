1c2fcc83c5ec6d2ca323d4178c59811541da2eec
DStark <hindash@email.arizona.edu>
2019-12-16 20:18:09 -0700

    functioning do-what-it-says

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var axios = require("axios");
 var moment = require("moment");
 var command = process.argv[2];
 var input = process.argv.slice(3).join(" ");
-// node liri.js concert - this < input / band name here >
 
 // begin api function for concert-this
 
@@ -42,7 +41,6 @@ function getArtist (artist)
 {
   return artist.name;
 };
-
 function findSong (song)
 {
   if (song === undefined) {
@@ -76,7 +74,48 @@ function findSong (song)
   };
 }
 // end spotify
+// start movie-this OMDB
+function movie (parameter)
+{
+  var movieName;
+  if (parameter === undefined) {
+    movieName = "Mr. Nobody";
+  } else {
+    movieName = parameter;
+  }
+  var url = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
 
+  axios.get(url).then(function (response)
+  {
+    var data = response.data;
+
+    console.log("Title: " + data.Title);
+    console.log("Year: " + data.Year);
+    console.log("Rated: " + data.Rated);
+    console.log("IMDB Rating: " + data.imdbRating);
+    console.log("Country: " + data.Country);
+    console.log("Language: " + data.Language);
+    console.log("Plot: " + data.Plot);
+    console.log("Actors: " + data.Actors);
+    console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value);
+  })
+}
+// function for running command based on text file (random.txt)
+function DoWhatItSays ()
+{
+  fs.readFile("random.txt", "utf8", function (error, data)
+  {
+    console.log(data);
+
+    var dataArray = data.split(",");
+
+    if (dataArray.length === 2) {
+      pick(dataArray[0], dataArray[1]);
+    } else if (dataArray.length === 1) {
+      pick(dataArray[0]);
+    }
+  });
+};
 
 // switch statement (if/else) or conditional statement
 var select = function (caseData, functionData)
@@ -88,7 +127,8 @@ var select = function (caseData, functionData)
       break;
     case "movie-this": movie(functionData);
       break;
-
+    case "do-what-it-says": DoWhatItSays();
+      break;
     // enter next case data here . (case, command and then the break)
     default: console.log("Error")
   }
