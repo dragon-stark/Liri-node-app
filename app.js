593273f9b8a805b7302f483be452d79d55dfc8dc
DStark <hindash@email.arizona.edu>
2020-01-30 13:16:12 -0700

    app updates with keys

--- app.js
+++ app.js
@@ -7,57 +7,56 @@ var axios = require("axios");
 var moment = require("moment");
 var command = process.argv[2];
 var input = process.argv.slice(3).join(" ");
-
+require("dotenv").config();
 // begin api function for concert-this
 
-var bandsintown = function (band)
-{
-  axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp").then(function (response)
-  {
-    var data = response.data;
-    for (var i = 0; i < data.length; i++) {
-
-      if (!data.length) {
-        console.log("Sorry, nothing found for " + input);
-        return;
-      }
-      console.log("appearences for " + input + " near you:");
+var bandsintown = function(band) {
+  axios
+    .get(
+      "https://rest.bandsintown.com/artists/" +
+        input +
+        "/events?app_id=codingbootcamp"
+    )
+    .then(function(response) {
+      var data = response.data;
       for (var i = 0; i < data.length; i++) {
-        var band = data[i];
+        if (!data.length) {
+          console.log("Sorry, nothing found for " + input);
+          return;
+        }
+        console.log("appearences for " + input + " near you:");
+        for (var i = 0; i < data.length; i++) {
+          var band = data[i];
 
-        console.log(`
+          console.log(`
          Venue: ${data[i].venue.name}
          Location: ${data[i].venue.city} ${data[i].venue.region}
-         Date: ${moment(data[i].datetime).format("MM/DD/YYYY")}`
-        )
+         Date: ${moment(data[i].datetime).format("MM/DD/YYYY")}`);
+        }
       }
-    }
-  }
-  )
-}
+    });
+};
 // end bandsintown
 
-function getArtist (artist)
-{
+function getArtist(artist) {
   return artist.name;
-};
-function findSong (song)
-{
+}
+function findSong(song) {
   if (song === undefined) {
-    song = "The Sign"
+    song = "The Sign";
   }
 
   // beginning of API call
 
   {
-    spotify.search({
-      type: "track",
-      query: song
-    },
-      function (err, data)
+    spotify.search(
       {
+        type: "track",
+        query: song
+      },
+      function(err, data) {
         if (err) {
-          console.log("Error Occurred: " + err)
+          console.log("Error Occurred: " + err);
         }
 
         var music = data.tracks.items;
@@ -71,22 +70,23 @@ function findSong (song)
         }
       }
     );
-  };
+  }
 }
 // end spotify
 // start movie-this OMDB
-function movie (parameter)
-{
+function movie(parameter) {
   var movieName;
   if (parameter === undefined) {
     movieName = "Mr. Nobody";
   } else {
     movieName = parameter;
   }
-  var url = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+  var url =
+    "http://www.omdbapi.com/?t=" +
+    movieName +
+    "&y=&plot=full&tomatoes=true&apikey=trilogy";
 
-  axios.get(url).then(function (response)
-  {
+  axios.get(url).then(function(response) {
     var data = response.data;
 
     console.log("Title: " + data.Title);
@@ -98,13 +98,11 @@ function movie (parameter)
     console.log("Plot: " + data.Plot);
     console.log("Actors: " + data.Actors);
     console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value);
-  })
+  });
 }
 // function for running command based on text file (random.txt)
-function DoWhatItSays ()
-{
-  fs.readFile("random.txt", "utf8", function (error, data)
-  {
+function DoWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
     console.log(data);
 
     var dataArray = data.split(",");
@@ -115,28 +113,30 @@ function DoWhatItSays ()
       pick(dataArray[0]);
     }
   });
-};
+}
 
 // switch statement (if/else) or conditional statement
-var select = function (caseData, functionData)
-{
+var select = function(caseData, functionData) {
   switch (caseData) {
-    case "concert-this": bandsintown(functionData);
+    case "concert-this":
+      bandsintown(functionData);
       break;
-    case "spotify-this": findSong(functionData);
+    case "spotify-this":
+      findSong(functionData);
       break;
-    case "movie-this": movie(functionData);
+    case "movie-this":
+      movie(functionData);
       break;
-    case "do-what-it-says": DoWhatItSays();
+    case "do-what-it-says":
+      DoWhatItSays();
       break;
     // enter next case data here . (case, command and then the break)
-    default: console.log("Error")
+    default:
+      console.log("Error");
   }
 };
-var execute = function (action, parameter)
-{
-  select(action, parameter)
+var execute = function(action, parameter) {
+  select(action, parameter);
 };
 
-execute(process.argv[2], process.argv.slice(3).join(" "))
-  ;
+execute(process.argv[2], process.argv.slice(3).join(" "));
